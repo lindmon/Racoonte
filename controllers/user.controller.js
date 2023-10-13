f875a0712ef8fb03927f6909afe2347bf6f32196
lindmon <m.eryzhenskaia@gmail.com>
2023-10-13 10:18:48 +0200

    controller for follow route added, first version

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -45,6 +45,48 @@ exports.deleteUser = async(req, res) => {
         console.log('user deleted');
     }catch(err){
         return res.status(500).json( {message: err});
-        console.log('user is not deleted');
+    }
+}
+
+exports.follow = async(req,res) => {
+    if(!ObjectID.isValid(req.params.id)||!ObjectID.isValid(req.body.idToFollow))
+        return res.status(400).send('ID unknown:' + req.params.id + req.body.idToFollow);
+    try {
+         await UserModel.findByIdAndUpdate(
+            req.params.id,
+            {$addToSet: 
+                {following: req.body.idToFollow
+                }
+            },
+            {new:true, upsert:true
+            },
+            (err,docs) => {
+                if(!err) res.status(201).json(docs);
+                else return res.status(400).json(err);
+            }
+        );
+    //Adding to the following list
+        await UserModel.findByIdAndUpdate(
+            req.body.idToFollow,
+            {$addToSet: 
+                {followers: req.params.id
+                }
+            },
+            (err, docs) => {
+                if(err) return res.status(400).json(err);
+            },
+        );
+    } catch (err) {
+        return res.status(500).json( {message: err});
+    }
+}
+
+exports.unfollow = async(req,res) => {
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown:' + req.params.id);
+    try {
+        
+    } catch (err) {
+        return res.status(500).json( {message: err});
     }
 }
